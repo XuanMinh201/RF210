bfef871a9a873ffb597d8bd8dbf0e0c18747ae7b
xuanminh <75436464+XuanMinh201@users.noreply.github.com>
2023-09-28 11:21:23 +0700

    Update DeepSleep.ino

--- DeepSleep/DeepSleep.ino
+++ DeepSleep/DeepSleep.ino
@@ -1,19 +1,30 @@
 void setup() {
-  Serial.begin(115200);
-  // disnable LDO Rak
+
+  // disable LDO Rak
   pinMode (10, OUTPUT);
   digitalWrite(10, LOW);
-  //Disnable GPS 
+  //Disnable GPS
   pinMode (1, OUTPUT);
-  digitalWrite(10, LOW);
-  pinMode (0, OUTPUT);
-  digitalWrite(10, LOW);  
+  digitalWrite(1, LOW);
+  
 }
 
 void loop() {
+  pinMode (10, OUTPUT);
+  digitalWrite(10, LOW);
+  //Disnable GPS
+  pinMode (1, OUTPUT);
+  digitalWrite(1, LOW);
 
-  // wake up 1 second later and then go into deep sleep
   esp_sleep_enable_timer_wakeup(10000000); // 10 sec
-  esp_deep_sleep_start(); 
-  // we never reach here
+  
+  delay(1000);
+  gpio_hold_en((gpio_num_t)10);
+  gpio_hold_en((gpio_num_t)1);
+  gpio_deep_sleep_hold_en();
+  // wake up 1 second later and then go into deep sleep
+  esp_sleep_enable_timer_wakeup(100000000); // 10 sec
+
+  esp_deep_sleep_start();
+  
 }
